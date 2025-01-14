59e6851ade2b404fc078e3d73424080f86e54725
kabin55 <shresthakabin46@gmail.com>
2025-01-14 10:02:01 +0545

    ..

--- movieRoute.js
+++ movieRoute.js
@@ -73,18 +73,5 @@ module.exports = router;
 // git branch -M main
 // git push -u origin main
 
-// hint: Using 'master' as the name for the initial branch. This default branch name
-// hint: is subject to change. To configure the initial branch name to use in all
-// hint: of your new repositories, which will suppress this warning, call:
-// hint:
-// hint:   git config --global init.defaultBranch <name>
-// hint:
-// hint: Names commonly chosen instead of 'master' are 'main', 'trunk' and
-// hint: 'development'. The just-created branch can be renamed via this command:
-// hint:
-// hint:   git branch -m <name>
-// hint:   git branch -m <name>
-// Initialized empty Git repository in /home/groot/Documents/code/class/node_curd_try/.git/
-// [master (root-commit) 4e38d82] first commit
-//  1 file changed, 2 insertions(+)
-//  create mode 100644 README.md
+// 77302A96
+// C53A-196F
